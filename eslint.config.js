d19626f79fef302e5324eee820296e83b1d1b72d
it-ermolaev <it.ermolaev@gmail.com>
2025-07-31 20:33:37 +0300

    chore: configure ESLint, Prettier, and import sorting with FSD aliases

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,44 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import eslint from '@eslint/js'
+import * as importPlugin from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier/recommended'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  eslint.configs.recommended,
+  prettierPlugin,
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
+  tseslint.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{js,ts,tsx}'],
+    ignores: ['dist'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: { import: importPlugin, 'simple-import-sort': simpleImportSortPlugin },
+    rules: {
+      'import/first': 'error',
+      'import/no-cycle': 'warn',
+      'import/no-duplicates': 'error',
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react', '^react-dom', '^@?\\w'],
+            ['^@app', '^@pages', '^@widgets', '^@features', '^@entities', '^@shared'],
+            ['^@', '^\\w'],
+            ['^\\u0000', '^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            ['\\.css$', '\\.scss$'],
+          ],
+        },
+      ],
     },
   },
 ])
